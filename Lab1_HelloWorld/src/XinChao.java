9e94a878aca867711d801df90799f488f633a942
hoangphucnguyen580-boop <hoangphucnguyen580@gmail.com>
2026-08-01 20:07:38 +0700

    Chương trình java đầu tiên
    Xin Chào Java

---
+++ Lab1_HelloWorld/src/XinChao.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Lenh in ra màn hình
+        System.out.println("Xin chào thế giới Java!");
+        System.out.println("Chào mừng đến với lập trình Java");
+        
+        // In kết quả tính toán
+        System.out.println("5 + 3 = " + (5 + 3));
+
+	}
+
+}
