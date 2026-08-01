f557e8558ba0cb0de9103791c6a036446b3117a0
hoangphucnguyen580-boop <hoangphucnguyen580@gmail.com>
2026-08-01 20:49:14 +0700

    Lab1_Bài3
    In kết quả các phép tính: cộng, trừ, nhân, chia

---
+++ Lab1_HelloWorld/src/TinhToan.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class TinhToan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Khai báo biến
+		int a = 10;
+		int b = 5;
+		
+		//Các phép tính
+		int cong = a + b;
+		int tru = a - b;
+		int nhan = a * b;
+		double chia = a / b;
+		
+		//In kết quả
+		System.out.println("Cộng: " + cong);
+		System.out.println("Trừ: " + tru);
+		System.out.println("Nhân: " + nhan);
+		System.out.println("Chia: " + chia);
+
+	}
+
+}
