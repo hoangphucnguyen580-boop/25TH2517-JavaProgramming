1aacceeab6c29efe90933cc88350ffba326377f7
hoangphucnguyen580-boop <hoangphucnguyen580@gmail.com>
2026-08-01 20:54:52 +0700

    Lab1_Bài4
    Code và chay các ví dụ HelloWord,

---
+++ Lab1_HelloWorld/src/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // Text blocks với """
+        String greeting = """
+            ======================
+            HELLO, MODERN JAVA!
+            ======================
+            by Nguyễn Hoàng Phúc NTU
+            ======================
+            """;
+        System.out.println(greeting);
+
+	}
+
+}
