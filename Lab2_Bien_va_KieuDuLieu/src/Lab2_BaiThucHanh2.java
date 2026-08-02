de085e6320ab928a7a7d5a7b486f70454aed9ee4
hoangphucnguyen580-boop <hoangphucnguyen580@gmail.com>
2026-08-02 08:08:50 +0700

    Lab2_Bài thực hành 2
    Chuyển đổi nhiệt độ

---
+++ Lab2_Bien_va_KieuDuLieu/src/Lab2_BaiThucHanh2.java
@@ -0,0 +1,17 @@
+
+public class Lab2_BaiThucHanh2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Chuyển từ Celsius sang Fahrenheit
+        // Công thức: F = (C × 9/5) + 32
+		
+		double celsius = 30.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        
+        //30.0 + °C = + 86.0 + °F"
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+
+	}
+
+}
