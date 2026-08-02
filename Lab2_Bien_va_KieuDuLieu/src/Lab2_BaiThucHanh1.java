e46b6445249167de8a6e0cc679c0f6734f206b38
hoangphucnguyen580-boop <hoangphucnguyen580@gmail.com>
2026-08-02 07:42:54 +0700

    Lab2_Bài Thực Hành 1
    Khai báo và tính toán

---
+++ Lab2_Bien_va_KieuDuLieu/src/Lab2_BaiThucHanh1.java
@@ -0,0 +1,23 @@
+
+public class Lab2_BaiThucHanh1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Khai báo biến
+		int a = 15;
+		int b = 9;
+		double c = 3.5;
+		
+		//Phéo tính
+		int tong = a + b;
+        double thuong = a / c;
+        int phandu = a % b;
+        
+        //In kết quả
+        System.out.println("Tổng: " + tong);
+        System.out.println("Thương: " + String.format("%.2f", thuong));
+        System.out.println("Phần dư: " + phandu);
+
+	}
+
+}
